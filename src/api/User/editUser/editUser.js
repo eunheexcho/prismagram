b693b5dea9c08ba1fcfdbc92211af25ff322cb93
eunheexcho <vialactea128@gmail.com>
2020-02-18 23:05:04 +0900

    avatar added

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -4,14 +4,14 @@ export default {
     Mutation: {
         editUser: async (_, args, {request, isAuthenticated}) => {
             isAuthenticated(request);
-            const {username, email, firstName, lastName, bio} = args;
+            const {username, email, firstName, lastName, bio, avatar} = args;
             const {user} = request;
             return prisma.updateUser({
                 where: {
                     id: user.id
                 },
                 data: {
-                    username, email, firstName, lastName, bio
+                    username, email, firstName, lastName, bio, avatar
                 }
             })
         }
