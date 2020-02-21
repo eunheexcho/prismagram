117010ed7f66189d9594b2c73aa00a0d579046da
eunheexcho <vialactea128@gmail.com>
2020-02-22 04:31:54 +0900

    seeRoom

--- src/fragments.js
+++ src/fragments.js
@@ -34,11 +34,25 @@ export const FULL_POST_FRAGMENT = `
     }
 `;
 
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to {
+        ${USER_FRAGMENT}
+    }
+    from {
+        ${USER_FRAGMENT}
+    }
+`;
+
 export const ROOM_FRAGMENT = `
     fragment RoomParts on Room{
         id
         participants {
             ${USER_FRAGMENT}
+        }
+        messages {
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
