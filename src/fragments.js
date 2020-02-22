8d8e0c4f1554c9b4d2a50a64a8d2c5352decff39
eunheexcho <vialactea128@gmail.com>
2020-02-23 02:38:41 +0900

    Kill the fragments

--- src/fragments.js
+++ src/fragments.js
@@ -17,23 +17,6 @@ export const FILE_FRAGMENT = `
     url
 `;
 
-export const FULL_POST_FRAGMENT = `
-    fragment PostParts on Post{
-        id
-        location
-        caption
-        files {
-            ${FILE_FRAGMENT}
-        }
-        comments {
-            ${COMMENT_FRAGMENT}
-        }
-        user {
-            ${USER_FRAGMENT}
-        }
-    }
-`;
-
 export const MESSAGE_FRAGMENT = `
     id
     text
